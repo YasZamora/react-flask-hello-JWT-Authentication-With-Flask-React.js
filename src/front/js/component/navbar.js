7d46324b56e45f28e7d6b90597e2147e130e6c47
YasZamora <yzamo001@gmail.com>
2022-03-20 22:52:03 +0000

    implementación logout da problema el cors

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  let history = useHistory();
+  const logoutHandler = () => {
+    const token = sessionStorage.getItem("token");
+    const response = fetch(process.env.BACKEND_URL + "/api/logout", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "aplication/json",
+        Authorization: "Bearer " + token,
+      },
+    });
+    sessionStorage.clear();
+    history.push("/login");
+  };
+
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container">
+        <Link to="/">
+          <span className="navbar-brand mb-0 h1">React Boilerplate</span>
+        </Link>
+        <div className="ml-auto">
+          {/* <Link to="/login"> */}
+          <button className="btn btn-primary" onClick={logoutHandler}>
+            Logout
+          </button>
+          {/* </Link> */}
+        </div>
+      </div>
+    </nav>
+  );
 };
